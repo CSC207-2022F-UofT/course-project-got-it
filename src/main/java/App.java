cf61210455ca441025e9919fc6adac435a0b728d
Arjun Saili <arjun7.saili@gmail.com>
2022-11-21 03:26:52 -0500

    Create temporary login screen view

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,21 @@
+import screens.LoginScreen;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class App {
     public static void main(String[] args){
-        SwingUtilities.invokeLater(new Runnable(){
-            @Override
-            public void run() {
-                MainWindow window = new MainWindow();
-                window.showLogin();
-            }
-        });
+        JFrame application = new JFrame("Got It");
+        CardLayout cardLayout = new CardLayout();
+        JPanel screens = new JPanel(cardLayout);
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        LoginScreen login = new LoginScreen();
+        screens.add(login, "login");
+        cardLayout.show(screens, "login");
+        application.add(screens);
+        application.pack();
+        application.setVisible(true);
+        String mongoURI = System.getenv("MONGOURI");
+        System.out.println(mongoURI);
     }
 }
---
+++ src/main/java/screens/LoginScreen.java
@@ -0,0 +1,40 @@
+package screens;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginScreen extends JPanel implements ActionListener {
+
+    /*LoginController controller;*/
+
+    public LoginScreen(/*LoginController controller*/){
+        /*this.controller = controller;*/
+        JLabel title = new JLabel("Login");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel usernameLabel = new JLabel("Username");
+        JTextField usernameField = new JTextField(15);
+        JLabel passwordLabel = new JLabel("Password");
+        JTextField passwordField = new JTextField(15);
+        JPanel usernamePanel = new JPanel();
+        usernamePanel.add(usernameLabel);
+        usernamePanel.add(usernameField);
+        JPanel passwordPanel = new JPanel();
+        passwordPanel.add(passwordLabel);
+        passwordPanel.add(passwordField);
+        JButton loginBtn = new JButton("login");
+        loginBtn.addActionListener(this);
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.add(title);
+        this.add(usernamePanel);
+        this.add(passwordPanel);
+        this.add(loginBtn);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getActionCommand());
+    }
+
+}
