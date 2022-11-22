c06e0e8a77fac8af99fd213b4ad07963d3cb0d06
Arjun Saili <arjun7.saili@gmail.com>
2022-11-21 23:52:52 -0500

    Connect to MongoDB and create query/find function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,7 @@
+import LoginUseCase.LoginDBGateway;
+import LoginUseCase.LoginInputBoundary;
+import LoginUseCase.LoginInteractor;
+import screens.LoginController;
 import screens.LoginScreen;
 
 import javax.swing.*;
@@ -5,17 +9,19 @@ import java.awt.*;
 
 public class App {
     public static void main(String[] args){
+        String mongoURI = System.getenv("MONGOURI");
         JFrame application = new JFrame("Got It");
         CardLayout cardLayout = new CardLayout();
         JPanel screens = new JPanel(cardLayout);
         application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        LoginScreen login = new LoginScreen();
+        LoginDBGateway dbGateway = new DatabaseUser(mongoURI);
+        LoginInputBoundary inputBoundary = new LoginInteractor(dbGateway);
+        LoginController loginController = new LoginController(inputBoundary);
+        LoginScreen login = new LoginScreen(loginController);
         screens.add(login, "login");
         cardLayout.show(screens, "login");
         application.add(screens);
         application.pack();
         application.setVisible(true);
-        String mongoURI = System.getenv("MONGOURI");
-        System.out.println(mongoURI);
     }
 }
