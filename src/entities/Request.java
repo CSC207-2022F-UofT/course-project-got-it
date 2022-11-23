f35e863bbdf3cb5423c282589f6d23b99fed26b5
haileyngg <79030515+haileyngg@users.noreply.github.com>
2022-11-23 01:56:32 -0500

    Add files via upload
    Request entity

---
+++ src/entities/Request.java
@@ -0,0 +1,44 @@
+package entities;
+
+import java.math.BigDecimal;
+
+public class Request {
+
+    private String driver;
+    private String user;
+    private double distance;
+    private BigDecimal price;
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+}
