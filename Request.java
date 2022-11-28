895a5dbc6e7601a10add21a582e9b416d39a8afc
shahi7 <69171919+shahi7@users.noreply.github.com>
2022-11-28 15:58:30 -0500

    Make Request Use Case
    Incomplete. Request entity unfinished, adding request info to DB to be implemented, unfinished screen/controller.

---
+++ Request.java
@@ -0,0 +1,17 @@
+package MakeRequestUseCase;
+
+// incomplete;
+public class Request {
+    private String driver, req_loc, item_loc, description, requester;
+
+    public Request(requester, item_loc, description) {
+        // get user data from user methods
+        this.requester = requester;
+        this.req_loq = req_loq;
+        this.driver = driver;
+        this.item_loc = item_loc;
+        this.description = description;
+    }
+
+    public getReq
+}
