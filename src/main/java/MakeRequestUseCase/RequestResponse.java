8142ab2815249c5d668141118d79ceed8f74fbdd
Arjun Saili <arjun7.saili@gmail.com>
2022-12-02 13:17:24 -0500

    Add Request Response from make request branch, update imports

---
+++ src/main/java/MakeRequestUseCase/RequestResponse.java
@@ -0,0 +1,19 @@
+package MakeRequestUseCase;
+
+public class RequestResponse {
+
+    private boolean validInput;
+
+    public RequestResponse(boolean validInput){
+        this.validInput = validInput;
+    }
+
+    public boolean isValidInput() {
+        return validInput;
+    }
+
+    public void setValidInput(boolean validInput) {
+        this.validInput = validInput;
+    }
+
+}
