5d95df294cca54d60f422325596d473347befc7a
Arjun Saili <arjun7.saili@gmail.com>
2022-12-02 13:12:23 -0500

    Import RequestController from Hailey and Aziz's  PR

---
+++ src/main/java/MakeRequestUseCase/RequestController.java
@@ -0,0 +1,19 @@
+package MakeRequestUseCase;
+import MakeRequestUseCase.RequestInputBoundary;
+import MakeRequestUseCase.RequestRequest;
+import MakeRequestUseCase.RequestResponse;
+
+public class RequestController {
+
+    final RequestInputBoundary inputBoundary;
+    public RequestController(RequestInputBoundary inputBoundary){
+        this.inputBoundary = inputBoundary;
+    }
+
+    RequestMadeScreen request(String req_loc, String item_loc, String description, String requester){
+        RequestRequest request = new RequestRequest(req_loc, item_loc, description, requester);
+        RequestResponse response = inputBoundary.request(request);
+        RequestMadeScreen updatedScreen = new RequestMadeScreen(response);
+        return updatedScreen;
+    }
+}
