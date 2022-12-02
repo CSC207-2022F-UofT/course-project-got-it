9f428c34b6d08a19f44e7eca7825c73d9349f797
Arjun Saili <arjun7.saili@gmail.com>
2022-12-01 19:17:29 -0500

    Add cooridnates and getter/setters to registerdbrequest

--- src/main/java/RegisterUseCase/RegisterDBRequest.java
+++ src/main/java/RegisterUseCase/RegisterDBRequest.java
@@ -3,12 +3,28 @@ package RegisterUseCase;
 public class RegisterDBRequest {
     private final String email;
     private String password;
-
-    public RegisterDBRequest(String email, String password){
+    private int longitude;
+    private String name;
+    private int latitude;
+    public RegisterDBRequest(int[] coords, String email, String password, String name){
         this.email = email;
         this.password = password;
+        this.longitude = coords[0];
+        this.latitude = coords[1];
+        this.name = name;
     }
 
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public String getName(){
+        return this.name;
+    }
     public String getEmail(){
         return email;
     }
