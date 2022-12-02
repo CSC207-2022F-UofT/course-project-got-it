58c0c69752e8acb0027e2a9e89c3ff9726f443e0
Arjun Saili <arjun7.saili@gmail.com>
2022-12-02 13:54:16 -0500

    Use predefined db gateway, create new function to validate fields

--- src/main/java/MakeRequestUseCase/RequestInteractor.java
+++ src/main/java/MakeRequestUseCase/RequestInteractor.java
@@ -1,21 +1,33 @@
 package MakeRequestUseCase;
 
+import dbGateway.DatabaseGateway;
 import entities.User;
+
+import java.util.List;
 
 public class RequestInteractor implements RequestInputBoundary{
 
-    private final RequestDBGateway gateway;
-    public RequestInteractor(RequestDBGateway dbGateway){
+    private final DatabaseGateway gateway;
+    public RequestInteractor(DatabaseGateway dbGateway){
         this.gateway = dbGateway;
     }
     @Override
     public RequestResponse request(RequestRequest requestRequest) { //check response validitiy
-        if(requestRequest.inputs().count('') == 0){ // checks if any field is empty
-            gateway.storeRequestInfo(); // store in DB (TO BE implemented)
+        if(checkForAnyEmptyField(requestRequest.inputs())){ // checks if any field is empty
+            gateway.storeRequestInfo(requestRequest); // store in DB (TO BE implemented)
             return new RequestResponse(true);
         }
         else{ // if any required field is blank
             return new RequestResponse(false);
         }
+    }
+
+    public boolean checkForAnyEmptyField(List inputs){
+        for (Object input : inputs) {
+            if (input == ""){
+                return false;
+            }
+        }
+        return true;
     }
 }
