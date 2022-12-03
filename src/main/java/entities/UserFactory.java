0f5c6462eea0ee1bbf92ddf3fd120050688b92e2
Arjun Saili <arjun7.saili@gmail.com>
2022-12-03 16:45:39 -0500

    update user factory to receive double instead of int

--- src/main/java/entities/UserFactory.java
+++ src/main/java/entities/UserFactory.java
@@ -1,7 +1,7 @@
 package entities;
 
 public class UserFactory {
-    public User create(int[] coords, String userEmail, String password, String name){
-        return new User(coords, userEmail, password, name, false);
+    public User create(double[] coords, String userEmail, String password, String name){
+        return new User(coords, userEmail, password, name);
     }
 }
