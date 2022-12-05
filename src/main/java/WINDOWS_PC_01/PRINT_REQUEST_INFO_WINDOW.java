ed8bb5af62f159e62cdcde6da8ae2f294d5a9078
Pranky1247 <113070816+Pranky1247@users.noreply.github.com>
2022-12-05 17:26:14 -0500

    I added some features into the Request info window and components need to be added into the JLayeredPane to actually work.

--- src/main/java/WINDOWS_PC_01/PRINT_REQUEST_INFO_WINDOW.java
+++ src/main/java/WINDOWS_PC_01/PRINT_REQUEST_INFO_WINDOW.java
@@ -1,4 +1,27 @@
 package WINDOWS_PC_01;
 
-public class PRINT_REQUEST_INFO_WINDOW {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PRINT_REQUEST_INFO_WINDOW extends JFrame implements ActionListener {
+    private JTextField Item_Location;
+    private JTextField Item_Description;
+    private JTextField Item_Delivery_Location;
+    private JTextField Driver_Info;
+    private JLayeredPane LP;
+    public PRINT_REQUEST_INFO_WINDOW(){
+        this.LP = new JLayeredPane();
+        this.LP.setSize(600, 600);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(600,600);
+        this.add(LP);
+        LP.add();
+        this.setVisible(true);
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
