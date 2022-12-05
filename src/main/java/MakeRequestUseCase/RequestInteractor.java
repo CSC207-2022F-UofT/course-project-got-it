b19d46709b937a0bbc2cdf11f7f241f20c761614
Arjun Saili <arjun7.saili@gmail.com>
2022-12-05 09:31:15 -0500

    call API to get geocodes in interactor

--- src/main/java/MakeRequestUseCase/RequestInteractor.java
+++ src/main/java/MakeRequestUseCase/RequestInteractor.java
@@ -1,6 +1,8 @@
 package MakeRequestUseCase;
 
+import APIGateway.APIGateway;
 import DatabaseGateway.DatabaseGateway;
+import entities.Request;
 import entities.User;
 import screens.Presenter;
 
@@ -8,15 +10,20 @@ import java.util.List;
 
 public class RequestInteractor implements RequestInputBoundary{
 
-    private final DatabaseGateway gateway;
+    private final DatabaseGateway dbGateway;
+    private final APIGateway apiGateway;
     private final Presenter presenter;
-    public RequestInteractor(DatabaseGateway dbGateway, Presenter presenter){
-        this.gateway = dbGateway;
+    public RequestInteractor(APIGateway apiGateway, DatabaseGateway dbGateway, Presenter presenter){
+        this.apiGateway = apiGateway;
+        this.dbGateway = dbGateway;
         this.presenter = presenter;
     }
     @Override
-    public void request(RequestRequest requestRequest) { //check response validitiy
-        if(checkForAnyEmptyField(requestRequest.inputs()) && gateway.storeRequestInfo(requestRequest)){ // checks if any field is empty
+    public void request(RequestRequest requestRequest) {
+        Object[] itemGeoCode = this.apiGateway.getGeocode(requestRequest.getItem_loc());
+        Object[] deliveryGeoCode = this.apiGateway.getGeocode(requestRequest.getReq_loc());
+        Request requestObj = new Request(requestRequest.getItemName(), requestRequest.getDescription(), (Double[]) itemGeoCode, (Double[]) deliveryGeoCode, requestRequest.getDeliveryNotes());
+        if(checkForAnyEmptyField(requestObj.getDetails()) && dbGateway.storeRequestInfo(requestRequest)){
             this.presenter.showMakeRequestSuccess();
         }
         else{
@@ -24,7 +31,7 @@ public class RequestInteractor implements RequestInputBoundary{
         }
     }
 
-    public boolean checkForAnyEmptyField(List inputs){
+    public boolean checkForAnyEmptyField(Object[] inputs){
         for (Object input : inputs) {
             if (input == ""){
                 return false;
