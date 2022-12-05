2d64ace81ed5f59864ad9be70956863c8e1cefe1
pranavag10 <pranav.agrawal@mail.utoronto.ca>
2022-12-05 09:00:42 -0500

    updated register to account for user's parameters

--- src/main/java/RegisterUseCase/RegisterRequest.java
+++ src/main/java/RegisterUseCase/RegisterRequest.java
@@ -1,14 +1,46 @@
 package RegisterUseCase;
 
 public class RegisterRequest {
+    private double longitude;
+
+    private String name;
+
+    private double latitude;
     private String email;
     private String password;
     private String repeatPassword;
 
-    public RegisterRequest(String email, String password, String repeatPassword) {
+    public RegisterRequest(String name, double[] coords, String email, String password, String repeatPassword) {
+        this.name = name;
+        this.longitude = coords[0];
+        this.latitude = coords[1];
         this.email = email;
         this.password = password;
         this.repeatPassword = repeatPassword;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
     }
 
     public String getEmail() {
