f293482004ed8cd6f60a74b3646cd42d9f497fb2
pranavag10 <pranav.agrawal@mail.utoronto.ca>
2022-12-08 00:14:03 -0500

    handle merge

--- src/main/java/HomescreenUseCase/HomescreenInteractor.java
+++ src/main/java/HomescreenUseCase/HomescreenInteractor.java
@@ -1,18 +1,24 @@
 package HomescreenUseCase;
 
+import APIGateway.APIGateway;
 import entities.User;
 import screens.Presenter;
 
 public class HomescreenInteractor {
     private final Presenter presenter;
     private final User currentUser;
-    public HomescreenInteractor(Presenter presenter, User currentUser){
+    private final APIGateway APIGateway;
+    public HomescreenInteractor(Presenter presenter, User currentUser, APIGateway APIGateway){
         this.presenter = presenter;
         this.currentUser = currentUser;
+        this.APIGateway = APIGateway;
     }
 
     public void navigateProfile(){
-        this.presenter.showProfile(currentUser.getName(), currentUser.getaddress(),
+        double[] coords = {this.currentUser.getLongitude(), this.currentUser.getLatitude()};
+        String address = this.APIGateway.getAddress(coords);
+        System.out.println(address);
+        this.presenter.showProfile(currentUser.getName(), address,
                 currentUser.getEmail(), currentUser.getPassword());
     }
 
@@ -23,5 +29,8 @@ public class HomescreenInteractor {
     public void navigatePastrequests(){
         this.presenter.showPastRequests();
     }
+
+    public void navigateCurrentrequests() {this.presenter.showCurrentrequest();}
+
 }
 
