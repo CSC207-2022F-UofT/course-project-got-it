897df0ecdf9f88dd71b97fa7f473d08f9b003e7e
pranavag10 <pranav.agrawal@mail.utoronto.ca>
2022-12-08 00:14:34 -0500

    commmits

--- src/main/java/LoginUseCase/LoginInteractor.java
+++ src/main/java/LoginUseCase/LoginInteractor.java
@@ -1,25 +1,30 @@
 package LoginUseCase;
 
+import APIGateway.APIGateway;
 import DatabaseGateway.DatabaseGateway;
 import entities.User;
 import screens.Presenter;
 import java.util.HashMap;
 public class LoginInteractor implements LoginInputBoundary, Interactor{
 
-    private final DatabaseGateway gateway;
+    private final DatabaseGateway dbgateway;
     private User currentUser;
+    private APIGateway apiGateway;
     private final Presenter presenter;
 
-    public LoginInteractor(DatabaseGateway dbGateway, Presenter presenter, User currentUser){
+    public LoginInteractor(DatabaseGateway dbGateway, APIGateway apiGateway, Presenter presenter, User currentUser){
         this.currentUser = currentUser;
-        this.gateway = dbGateway;
+        this.apiGateway = apiGateway;
+        this.dbgateway = dbGateway;
         this.presenter = presenter;
     }
     @Override
     public void login(LoginRequest loginRequest) {
-        if(gateway.validateAndLogin(loginRequest.getEmail(), loginRequest.getPassword())){
-            HashMap<String, Object> dbUser = gateway.getLoggedInUser();
-            this.currentUser.setAddress( (String) dbUser.get("address"));
+        if(dbgateway.validateAndLogin(loginRequest.getEmail(), loginRequest.getPassword())){
+            HashMap<String, Object> dbUser = dbgateway.getLoggedInUser();
+            System.out.println(dbUser);
+            this.currentUser.setLongitude((double)dbUser.get("longitude"));
+            this.currentUser.setLatitude((double)dbUser.get("latitude"));
             this.currentUser.setName((String)dbUser.get("name"));
             this.currentUser.setPassword((String)dbUser.get("password"));
             this.currentUser.setEmail((String)dbUser.get("email"));
@@ -33,7 +38,7 @@ public class LoginInteractor implements LoginInputBoundary, Interactor{
             this.presenter.loginFailed();
         }
     }
-
+    public void back() {this.presenter.showPreviousScreen();}
     public void navigateSignup(){
         this.presenter.start();
     }
