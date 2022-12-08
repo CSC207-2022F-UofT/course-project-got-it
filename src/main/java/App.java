39d33708a921ac3610023ed326d930225285bf29
pranavag10 <pranav.agrawal@mail.utoronto.ca>
2022-12-08 00:13:16 -0500

    handle merge

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,7 @@ import DatabaseGateway.DatabaseGateway;
 import LoginUseCase.LoginInteractor;
 import ProfilescreenUseCase.ProfilescreenInteractor;
 import RegisterUseCase.RegisterInteractor;
+import UserResponseWindowUseCase.UserResponseInteractor;
 import entities.User;
 import entities.UserFactory;
 import HomescreenUseCase.HomescreenInteractor;
@@ -33,13 +34,16 @@ public class App implements PresenterObserver {
         this.apiGateway = new PositionStackAPI(apiKey);
         this.screenMap = new HashMap<>();
         this.screenMap.put(
-                "login", new LoginController(new LoginInteractor(this.dbGateway, this.presenter, this.currentUser)));
+                "login", new LoginController(new LoginInteractor(this.dbGateway, this.apiGateway, this.presenter, this.currentUser)));
         this.screenMap.put(
-                "register", new RegisterController(new RegisterInteractor(this.dbGateway, new UserFactory(), this.presenter)));
-        this.screenMap.put("homescreen", new screens.HomescreenController(new HomescreenInteractor(this.presenter, this.currentUser)));
-        this.screenMap.put("profilescreen", new screens.ProfilescreenController(new ProfilescreenInteractor(this.dbGateway,this.currentUser, this.presenter)));
+                "register", new RegisterController(new RegisterInteractor(this.dbGateway, this.apiGateway, new UserFactory(), this.presenter)));
+        this.screenMap.put("homescreen", new screens.HomescreenController(new HomescreenInteractor(this.presenter, this.currentUser, this.apiGateway)));
+        this.screenMap.put("profilescreen", new screens.ProfilescreenController(new ProfilescreenInteractor(this.dbGateway,
+                this.currentUser, this.presenter)));
         this.screenMap.put("makeRequest", new RequestController(
                 (RequestInputBoundary) new RequestInteractor(this.apiGateway, this.dbGateway, this.presenter), this.currentUser));
+        this.screenMap.put("loginFailed", new UserResponseController(new UserResponseInteractor(this.presenter)));
+        this.screenMap.put("registerFailed", new UserResponseController(new UserResponseInteractor(this.presenter)));
     }
 
     public void setWindow(JFrame window) {
@@ -59,10 +63,12 @@ public class App implements PresenterObserver {
     @Override
     public void updateScreen(String screenName, Screen screen) {
         this.window.setVisible(false);
+        setWindow((JFrame)screen);
         screen.setController(this.screenMap.get(screenName));
+        if(screen instanceof Backable){
+            ((Backable) screen).setBackController(new UserResponseController(new UserResponseInteractor(this.presenter)));
+        }
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.pack();
-        this.window = (JFrame)screen;
         this.window.setVisible(true);
     }
 }
