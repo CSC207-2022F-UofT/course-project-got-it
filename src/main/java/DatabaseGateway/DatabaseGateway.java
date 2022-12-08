157965025e0a0c848156ab831f07f928beaa917f
Varchas Sharma <varchassharma23@gmail.com>
2022-12-08 18:59:12 -0500

    Implementing Javadoc to each relevant method and class.

--- src/main/java/DatabaseGateway/DatabaseGateway.java
+++ src/main/java/DatabaseGateway/DatabaseGateway.java
@@ -7,6 +7,10 @@ import entities.User;
 
 import java.util.HashMap;
 
+/**
+ * Interface for database to prevent strong coupling between the database and interior components
+ * Holds all functions that are required by components
+ */
 public interface DatabaseGateway {
     boolean validateAndLogin(String email, String password);
 
