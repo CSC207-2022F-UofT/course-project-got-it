55b35a41827e52c300a6a4d0c4fce8b6d82687ba
Varchas Sharma <varchassharma23@gmail.com>
2022-12-08 18:55:11 -0500

    Implementing Javadoc to each relevant method and class.

--- src/main/java/entities/UserFactory.java
+++ src/main/java/entities/UserFactory.java
@@ -1,7 +1,7 @@
 package entities;
 
 /**
- * Entity later for user factory that contains the new user information
+ * Entity later for user factory that contains the new user information that is being created
  */
 public class UserFactory {
     public User create(double longitude, double latitude, String userEmail, String password, String name){
