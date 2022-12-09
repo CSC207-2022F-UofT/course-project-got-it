acc20698529b860b5a5647e95e94bc656bc22ca2
pranavag10 <pranav.agrawal@mail.utoronto.ca>
2022-12-08 19:45:27 -0500

    commits

--- src/main/java/HomescreenUseCase/HomescreenInteractor.java
+++ src/main/java/HomescreenUseCase/HomescreenInteractor.java
@@ -1,17 +1,25 @@
 package HomescreenUseCase;
 //KEEP MINE
 import APIGateway.APIGateway;
+import DatabaseGateway.DatabaseGateway;
+import entities.Request;
 import entities.User;
 import screens.Presenter;
+
+import java.util.ArrayList;
 
 public class HomescreenInteractor {
     private final Presenter presenter;
     private final User currentUser;
     private final APIGateway APIGateway;
-    public HomescreenInteractor(Presenter presenter, User currentUser, APIGateway APIGateway){
+    private final DatabaseGateway dbGateway;
+
+    public HomescreenInteractor(Presenter presenter, User currentUser, APIGateway APIGateway,
+                                DatabaseGateway dbGateway){
         this.presenter = presenter;
         this.currentUser = currentUser;
         this.APIGateway = APIGateway;
+        this.dbGateway = dbGateway;
     }
 
     public void navigateProfile(){
@@ -27,10 +35,19 @@ public class HomescreenInteractor {
     }
 
     public void navigatePastrequests(){
-        this.presenter.showPastRequests();
+        ArrayList<Request> pastRequests = this.dbGateway.getPassedRequests(this.currentUser);
+        this.presenter.showPastRequests(pastRequests);
     }
 
-    public void navigateCurrentrequests() {this.presenter.showCurrentrequest();}
+    public void navigateCurrentrequests() {
+        ArrayList<Request> requests = this.dbGateway.getRequests(this.currentUser);
+        Request request = requests.get(0);
+        String itemLocation = this.APIGateway.getAddress(request.getItemAddress());
+        String deliveryLocation = this.APIGateway.getAddress(request.getDeliveryAddress());
+        String driverName = this.dbGateway.getDriverName(request.getRequestId());
+        this.presenter.showCurrentrequest(request.getItemName(), request.getitemDescription(), itemLocation,
+                deliveryLocation, driverName);
+    }
 
 }
 
